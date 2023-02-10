4d04ea9744d7753370d5b6d4369b9e6561bbfde8
Farid Bagirov <makflay85zet@gmail.com>
2023-02-10 20:30:39 +0400

    add 02

---
+++ 02/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Enter your name , here: ");
+string username = Console.ReadLine();
+Console.WriteLine($"Привет, {username}!)");
